d8c6ae61cba4f9e2d32f9a979cde0fe03445670b
truong <ntruong@switch.tv>
2024-10-07 11:08:55 +0700

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+interface RootLayoutProps {
   children: React.ReactNode;
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
