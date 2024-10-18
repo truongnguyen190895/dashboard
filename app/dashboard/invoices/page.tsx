879cd7163ff7944efa9bdabc02038416700d980b
truong <ntruong@switch.tv>
2024-10-18 10:54:09 +0700

    add MUI

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,11 @@
+import { Suspense } from "react";
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
 
 interface InvoiceProps {
   searchParams?: {
@@ -16,6 +17,7 @@ interface InvoiceProps {
 export default async function Page({ searchParams }: InvoiceProps) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -29,7 +31,7 @@ export default async function Page({ searchParams }: InvoiceProps) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -8,7 +9,12 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <head>
+        <title>Tư vấn luật</title>
+      </head>
+      <AppRouterCacheProvider>
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </AppRouterCacheProvider>
     </html>
   );
 }
